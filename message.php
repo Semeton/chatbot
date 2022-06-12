26898bbd59c8b19c1f8c748bb4834b313fe6e5d3
Semeton <88825982+Semeton@users.noreply.github.com>
2022-06-12 21:27:35 +0100

    Changes to queries

--- message.php
+++ message.php
@@ -7,13 +7,13 @@ $getMesg = mysqli_real_escape_string($db, $_POST['text']);
 
 //checking user query to database query
 $check_data = "SELECT replies FROM chatbot WHERE messages LIKE '%$getMesg%'";
-$run_query = mysqli_query($db, $check_data) or die("Error");
+$query = mysqli_query($db, $check_data) or die("Error");
 
 // if user query matched to database query we'll show the reply otherwise it go to else statement
-if(mysqli_num_rows($run_query) > 0)
+if(mysqli_num_rows($query) > 0)
 {
     //fetching response from the database according to the user query
-    $fetch_data = mysqli_fetch_assoc($run_query);
+    $fetch_data = mysqli_fetch_assoc($query);
     //storing response to a varible which we'll send to ajax
     $response = $fetch_data['replies'];
     echo $response;
